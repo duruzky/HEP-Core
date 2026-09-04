6ccc3114fe9473c5ac17090015d2a479de85ab66
Duru <duruozkaya>
2026-09-04 20:12:57 +0300

    Add Linear Algebra (Vector) class and implement Composition architecture

--- main.cpp
+++ main.cpp
@@ -1,70 +1,93 @@
 #include <iostream>
-#include <fstream>
-using namespace std;
+#include <fstream> 
+#include <cmath> 
+using namespace std; 
 
+class Vector {
+    public:
+        double x, y, z ;
+    
+        Vector(double vx=0.0, double vy=0.0, double vz=0.0){
+            x=vx;
+            y=vy;
+            z=vz;
+        } 
+        double magnitude(){
+             return sqrt(x*x + y*y + z*z);
+        }
+        void display(){
+            cout << "Vector: (" << x << ", " << y << ", " << z << ")";
+        }
+
+};
 class Particle {
-private:
-int id;
-double x, y, z;
-double energy;
+    private:
+        int id;
+        Vector position;
+        double energy;
 
-public:
-Particle() {
-        id = 0; x = 0.0; y = 0.0; z = 0.0; energy = 0.0;
-    }
-Particle(int p_id, double p_x, double p_y, double p_z, double p_energy ) {
-        id = p_id; x = p_x; y = p_y; z = p_z; energy = p_energy;
-    }
-double getEnergy() {
-        return energy;
-    }
-
-void printInfo() {
-cout<< "Particle ID: " << id 
- << " | Position: (" << x << ", " << y << ", " << z << ") | Energy:"<< energy <<  endl;
-}
+    public:
+        Particle() {
+            id = 0;
+            energy = 0.0;
+        }
+    
+        Particle(int pid, double px, double py, double pz, double penergy){
+            id = pid;
+            position = Vector(px, py, pz);
+            energy = penergy;
+        }
+        double GetEnergy(){
+            return energy;
+        }
+        void printInfo(){
+            cout<< "Particle ID: " << id << endl;
+            cout<< "Position:";
+            position.display() ;
+            cout<< endl << "Magnitude:" << position.magnitude() << endl;
+            cout<< "Energy: " << energy << endl;
+        }
 };
 
 class Event {
-    private: 
-    int eventId;
-    int capacity;
-    int particleCount;
-    Particle* particles;
-
-public:
-    Event(int e_id, int e_capacity) {
-        eventId = e_id;
-        capacity = e_capacity;
-        particleCount = 0;
-        particles = new Particle[capacity];
-        cout << "Event " << eventId << " created. Memory allocated.\n";
-    }
-    ~Event() {
+    private:
+        int eventId;
+        int capacity;
+        int particleCount;
+        Particle* particles;
+    
+    public:
+       Event(int eid, int ecapacity) {
+            eventId = eid;
+            capacity = ecapacity;
+            particleCount = 0; 
+            particles = new Particle[capacity]; 
+       }
+       ~Event() {
         delete[] particles;
-        cout<< "Event " << eventId << " deleted. Memory cleared.\n";
-    }
-
-    void addParticle(int p_id, double x, double y, double z,double p_energy) {
-        if (particleCount < capacity) {
-            particles[particleCount] = Particle(p_id, x, y, z, p_energy);
-            particleCount++;
-        } else {
-            cout << "Error: Event capacity is full.\n";
+       }
+       
+       void addParticle(int pid, double x, double y, double z, double penergy) {
+            if (particleCount < capacity) {
+                particles[particleCount] = Particle(pid, x, y, z, penergy);
+                particleCount++;
+            } 
+            else {
+                cout << "Error: Event capacity is full.\n";
+            }
         }
-    }
 
-    void printEvent() {
-        cout << "Event ID: " << eventId << " Results\n";
-        for (int i = 0; i < particleCount; i++) {
-            particles[i].printInfo();
+        void printEvent() {
+            cout << "Event ID: " << eventId << " Results:\n";
+            for (int i = 0; i < particleCount; i++) {
+                particles[i].printInfo();
+            }
         }
-    }
 
-    void sortParticlesByEnergy() {
+        void sortParticlesByEnergy() {
         for (int i = 0; i < particleCount - 1; i++) {
             for (int j = 0; j < particleCount - i - 1; j++) {
-                if (particles[j].getEnergy() < particles[j + 1].getEnergy()) {
+                if (particles[j].GetEnergy() < particles[j + 1].GetEnergy()) {
                     Particle temp = particles[j];
                     particles[j] = particles[j + 1];
                     particles[j + 1] = temp;
@@ -72,37 +95,31 @@ public:
             }
         }
     }
-
 };
-
 int main() {
-    cout << "Initializing HEP-Core System.\n";
+    cout << "Initializing HEP-Core System...\n";
     Event collision1(67, 100);
-   
     
     ifstream file("data.txt");
     if (!file.is_open()) {
         cout << "Error: Could not open data.txt!\n";
         return 1;
     }
-    double x, y, z, energy;
+    
+    double x, y, z, enerji;
     int id_counter = 1;
-    cout<< "File opened,particles are loading to RAM.\n";
+    
+    cout << "File opened, particles are loading to RAM...\n\n";
 
-
-    while (file >> x >> y >> z >> energy) {
-        collision1.addParticle(id_counter, x, y, z, energy);
+    while (file >> x >> y >> z >> enerji) {
+        collision1.addParticle(id_counter, x, y, z, enerji);
         id_counter++;
     }
     file.close();
-    cout << "File closed.\n";
-
-    cout << (id_counter - 1) << " particles loaded successfully.\n\n";
-    cout << "Original Order:\n";
-    collision1.printEvent();
-    cout << "Sorting by Energy:\n";
     collision1.sortParticlesByEnergy();
     collision1.printEvent();
-    cout << "System shutting down.\n";
+    
+    cout << "\nSystem shutting down.\n";
     return 0;
 }
+    
